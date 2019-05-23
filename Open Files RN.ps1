d70e7c58de595458f513a43aae5c093fcd641e84
Sichno <39745752+Sichno@users.noreply.github.com>
2019-05-23 09:07:28 -0700

    Add files via upload

---
+++ Open Files RN.ps1
@@ -0,0 +1,8 @@
+﻿#This script is scheduled to start at midnight, This script outputs users who have left files open at that time. 
+$date = get-date -format mm-dd-yyyy
+Start-Transcript -Path "C:\Temp\OpenFiles_$date.txt" -IncludeInvocationHeader -Append
+    Write-Verbose "Log Entry"
+    $sessn = New-CIMSession –Computername pmi-vm01
+    Get-SMBOpenFile -CIMSession $sessn |Select-Object -Property ClientComputerName, ClientUserName, Path, FileId,SessionId , Permissions, ShareRelativePath 
+    Write-Error "log an error"
+Stop-Transcript
